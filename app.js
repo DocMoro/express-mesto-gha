b8039ff1fbc21f108f644d6c1ed5339387854ec2
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-01-06 17:28:26 +0300

    feat: создать роутеры пользователя

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bobeParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(bobeParser.json());
 
-mongoose.connect('mongodb://localhost:27017/mydb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const User = require('../models/user');
+
+router.get('/', (req, res) => {
+  User.find({})
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+});
+
+router.get('/:userId', (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+});
+
+router.post('/', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+});
+
+module.exports = router;
