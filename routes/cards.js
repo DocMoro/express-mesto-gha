b81a893009be0f2037b1202512f037ef576894f5
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-01-06 20:55:40 +0300

    feat: исключить время создания карт в ответе запросов

--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Card = require('../models/card');
 
 router.get('/', (req, res) => {
-  Card.find({})
+  Card.find({}).select(['-createdAt'])
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 });
@@ -10,13 +10,13 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const { name, link } = req.body;
   const { _id } = req.user;
-  Card.create({ name, link, owner: _id })
+  Card.create({ name, link, owner: _id }).select(['-createdAt'])
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 });
 
 router.delete('/:cardId', (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId)
+  Card.findByIdAndRemove(req.params.cardId).select(['-createdAt'])
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 });
@@ -26,7 +26,7 @@ router.put('/:cardId/likes', (req, res) => {
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
-  )
+  ).select(['-createdAt'])
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 });
@@ -36,7 +36,7 @@ router.delete('/:cardId/likes', (req, res) => {
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
-  )
+  ).select(['-createdAt'])
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 });
