69106e7e2d9335b0ca271713d85f0bf1288c8152
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-01-09 16:06:26 +0300

    feat: добавить поля email и password в схему пользователя

--- models/user.js
+++ models/user.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+          return isEmail(v);
+      },
+      message: 'Переданы некорректные данные',
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
   name: {
     type: String,
     required: true,
