b64da1b78e92b805bd6042005091b5fbf29cb386
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-01-10 12:59:59 +0300

    fix: исправить ошибки eslint 2

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use('/', (req, res) => {
-  next(new Error404(ERR_404));
+  return next(new Error404(ERR_404));
 });
 
 app.use((err, req, res, next) => {
