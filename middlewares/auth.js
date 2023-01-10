09a6ec7bbdc629f51f68d996025d91c386836379
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-01-10 09:08:56 +0300

    feat: создать мидлвэр авторизации

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const ERR_401 = 'Необходима авторизация';
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: ERR_401 });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ message: ERR_401 });
+  }
+
+  req.user = payload;
+
+  next();
+};
